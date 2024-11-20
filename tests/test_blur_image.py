36bd1220354e894342e94e98ce6241055678e00d
Vinicius <v206768@dac.unicamp.br>
2024-11-20 19:28:32 -0300

    lint

--- tests/test_blur_image.py
+++ tests/test_blur_image.py
@@ -1,18 +1,23 @@
 from PIL import Image, ImageFilter
 from scripts.blur_image import blur_image
 
+
 def test_blur_image(mocker):
     # Mock the Image.open method
     mock_image = mocker.MagicMock(spec=Image.Image)
-    mock_image.filter = mocker.MagicMock(side_effect=lambda f: f"blurred_with_{round(f.radius, 1)}")
+    mock_image.filter = mocker.MagicMock(
+        side_effect=lambda f: f"blurred_with_{round(f.radius, 1)}"
+    )
     mock_image_open = mocker.patch("PIL.Image.open", return_value=mock_image)
-    
+
     # Call the function under test
     mock_path = "dummy_path"
     blurred_images = blur_image(mock_path)
 
     # Assertions
-    mock_image_open.assert_called_once_with(mock_path)  # Ensure Image.open was called with the path
+    mock_image_open.assert_called_once_with(
+        mock_path
+    )  # Ensure Image.open was called with the path
     assert mock_image.filter.call_count == 9  # Ensure filter was called 9 times
 
     # Validate the blur levels applied
